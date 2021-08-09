34f2a6346e4a651a0201b6e6512017433042b830
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-08-10 00:45:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const fs = require("fs");
-const fetch = require("node-fetch");
 
 
 
