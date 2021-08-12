293dc63011e6259130e7c26dd49f6cc8784459ea
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-08-12 12:20:04 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -68,7 +68,7 @@ module.exports.run = async (client, message, args) => {
     .addField('System channel', `${sysChannel}`, true)
     .addField("Server Emojis", ":grin: ``" + emo.toString() + "``", true)
     .addField("Features", `-${message.guild.features}`)
-    .setImage(message.guild.bannerURL(size = "2048"))
+    .setImage(message.guild.bannerURL(size = 2048))
 	.setFooter('Formova', 'https://g.top4top.io/p_14877vn8y1.jpg');
     
     message.channel.send({ embeds: [info] })
